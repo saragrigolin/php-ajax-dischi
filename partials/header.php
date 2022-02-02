1456a6e6af8745c2d0648ac94eaa60c345883fbd
saragrigolin <sara.grigolin95@gmail.com>
2022-02-02 15:42:49 +0100

    create header

---
+++ partials/header.php
@@ -0,0 +1,5 @@
+<header>
+    <div class="container-fluid">
+        <img class="py-2" src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/2048px-Spotify_logo_without_text.svg.png" alt="">
+    </div>
+</header>
